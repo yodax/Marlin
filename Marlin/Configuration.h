7f76593f76a881b9969940f4891d93bb91dc1cb0
smith3d <68049512+smith3d@users.noreply.github.com>
2020-10-01 10:17:42 +0800

    add V427 comment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -129,6 +129,7 @@
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
   #define MOTHERBOARD BOARD_CREALITY_V4
+  //#define MOTHERBOARD BOARD_CREALITY_V427
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
